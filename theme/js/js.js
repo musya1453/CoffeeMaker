cf7ca292a144c32a8b9918883051848594091fba
Musaev Ruslan <musya.wow@gmail.com>
2017-05-01 15:22:08 +0300

    'section-top:

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,25 +1,37 @@
 (function ($) {
 
-  if (typeof Drupal != 'undefined') {
-    Drupal.behaviors.wcportalMobileTheme = {
-      attach: function (context, settings) {
-        init();
-      },
-
-      completedCallback: function () {
-        // Do nothing. But it's here in case other modules/themes want to override it.
-      }
-    }
-  }
-
-  $(function () {
-    if (typeof Drupal == 'undefined') {
-      init();
-    }
+  $(window).load(function(){
+    initScrollR();
+    initHeader();
   });
 
-  function init() {
+  function initScrollR() {
 
+    if(document.documentElement.classList.contains('tablet') || document.documentElement.classList.contains('mobile')) {
+      return;
+    }
+
+    var s = skrollr.init({
+      render: function (data) {
+
+      },
+      forceHeight: false,
+      smoothScrolling: false
+    });
+  }
+
+  function initHeader(){
+    var $header = $('.header-wrapper');
+
+    $(window).bind('scroll', function(){
+
+      var $scroll = $('body').scrollTop();
+      if($scroll>=100){
+        $header.addClass(('style-a'))
+      }else{
+        $header.removeClass(('style-a'))
+      }
+    })
   }
 
 })(jQuery);
