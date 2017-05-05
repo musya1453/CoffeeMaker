c68ea46d6bd48420daae466f461582db386ff2a3
Musaev Ruslan <musya.wow@gmail.com>
2017-05-05 17:37:51 +0300

    new par

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,13 +1,48 @@
 (function ($) {
 
-  $(window).load(function(){
+  $(window).load(function () {
     initScrollR();
     initHeader();
+    initFlexSlider();
+    initSliderPartners();
   });
+
+  function initFlexSlider() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      prevText: "",
+      nextText: "",
+      slideshowSpeed: 4000
+
+    });
+  }
+
+  function initSliderPartners() {
+    $('.section-partners .slider').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 6000
+    });
+
+    var $wrapper = $('.section-partners .slider .slick-track'),
+      $elems = $wrapper.find('>.slick-slide'),
+      heightEl = 0;
+
+    $elems.find('img').each(function () {
+      if ($(this).height() > heightEl) {
+        heightEl = $(this).height();
+      }
+    });
+
+    $elems.each(function () {
+      $(this).height(heightEl);
+    })
+  }
 
   function initScrollR() {
 
-    if(document.documentElement.classList.contains('tablet') || document.documentElement.classList.contains('mobile')) {
+    if (document.documentElement.classList.contains('tablet') || document.documentElement.classList.contains('mobile')) {
       return;
     }
 
@@ -20,15 +55,15 @@
     });
   }
 
-  function initHeader(){
+  function initHeader() {
     var $header = $('.header-wrapper');
 
-    $(window).bind('scroll', function(){
+    $(window).bind('scroll', function () {
 
       var $scroll = $('body').scrollTop();
-      if($scroll>=100){
+      if ($scroll >= 100) {
         $header.addClass(('style-a'))
-      }else{
+      } else {
         $header.removeClass(('style-a'))
       }
     })
